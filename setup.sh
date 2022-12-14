6397f1de9039b8f6e0347f8ff8e505c0f6851e23
Kattson Bastos <kattsonbastos@gmail.com>
2022-12-14 00:55:19 +0000

    feat: adding setup for dev environment

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+###
+# this file is used to setup the dev environment in Gitpod
+###
+
+echo "Installing Terraform.."
+###TODO
+
+echo "Installing AWS CLI.."
+###TODO
+
+echo "Installing terraform VSCode extension..."
+code --install-extension hashicorp.terraform
+
+echo "Setup finished!"
